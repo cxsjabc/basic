d9cb321d537dc56f0e1c4cfb52fa1bddb87bcd68
Xi Chen <511272827@qq.com>
2017-04-09 21:35:12 +0800

    test float effective digits
    Signed-off-by: Xi Chen <511272827@qq.com>

--- c/test_float.c
+++ c/test_float.c
@@ -12,6 +12,7 @@ void print_each_byte(unsigned char *p, int size)
 
 int main()
 {
+#if 0
     double d = 38414.4;
 	int i;
 	unsigned char *pc = (unsigned char *)&d;
@@ -20,5 +21,27 @@ int main()
 	memcpy(&i, &d, sizeof(int));
 	printf("%d %x\n", i, i);
 	print_each_byte(pc, sizeof(d));
+#endif
+#if 1
+	{
+		double d;
+		scanf("%lf", &d);
+		printf("%.16lf\n", d);
+		if(d > 0)
+			printf("%lf > 0\n", d);
+	}
+#endif
+
+#if 0
+	{
+		float d;
+		scanf("%f", &d);
+		printf("%f %.16f\n", d, d); // if you input: 123456789, the print may be not this value. Because the effective digit number contains the main part and digital part.
+		if(d > 0)
+			printf("%f > 0\n", d);
+		if(d > 123456789)
+			printf("%f > \n", d);
+	}
+#endif
 	return 0;
 }
