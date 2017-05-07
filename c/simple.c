907f4755c55be42e39fc78a887c5df0cdf84b670
Xi Chen <511272827@qq.com>
2017-05-07 16:32:27 +0800

    test INT_MAX + 1, INT_MIN - 1, float effective digits
    Signed-off-by: Xi Chen <511272827@qq.com>

--- c/simple.c
+++ c/simple.c
@@ -3,7 +3,31 @@
 int main()
 {
 	int a = '\12';
+#if 0
     printf("%d\n", a);
+#endif
+	
+#if 1
+	{
+		int a = 2147483647;
+		printf("a:%d, a+1:%d\n", a, a + 1);
+		a = -2147483648;
+		printf("a:%d, a-1:%d\n", a, a - 1);
+	}
+#endif
+
+#if 1
+	{
+		float f1 = 123456789.12345, f2 = 123456789.12345;
+		
+		while(1) {	
+			printf("%f %f\n", f1, f2);	
+			printf("%.10f %.10f\n", f1, f2);	
+			scanf("%f%f", &f1, &f2);
+			printf("%f %f\n", f1, f2);	
+		}
+	}
+#endif
 
     return 0;
 }
