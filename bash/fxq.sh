53a0031c685e46f72241706a0005f5c3638a226b
Xi Chen <511272827@qq.com>
2017-09-24 16:08:57 +0800

    rearrange folder
    Signed-off-by: Xi Chen <511272827@qq.com>

---
+++ bash/fxq.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# written by: xichen  511272827@qq.com
+
+echo "Who is the person that you want to meet:"
+read person
+echo "You want to meet $person!"
+echo "$person is also want to meet you!"
+
+
+exit 0
