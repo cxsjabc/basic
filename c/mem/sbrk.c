fa5949f0e70facc22e45e8e12cb92f08cb814b30
Xi Chen <511272827@qq.com>
2017-10-06 11:42:41 +0800

    sbrk
    Signed-off-by: Xi Chen <511272827@qq.com>

--- c/mem/sbrk.c
+++ c/mem/sbrk.c
@@ -1,13 +1,57 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <assert.h>
+#include <sys/resource.h>
 
+#include "common_local.h"
+
+int bss_end;
+
+char *pb;
+void show_program_break()
+{
+	void *p;
+
+	p = sbrk(0);
+	if(p != (void *)-1)
+		pb = p, printf("program break:%p\n", p);
+	else
+		printf("get program break error!\n");
+}
+
+/* seems on Mac, brk failed always! */
 int main()
 {
 	void *p;
+		
+	printf("bss_end:%p\n", (char *)&bss_end + 4);
+	show_program_break();
 	
+	if(brk((char *)pb - 1) == (void *)-1)	// minus 1 to program break: error 
+		perror("brk error");
+
+	show_program_break();
+
+	p = malloc(32);
+	assert(p);
+	
+	show_program_break();
+
+	if(brk((char *)pb - 10) == (void *)-1)	// error
+		perror("brk error");
+	if(brk((char *)pb) == (void *)-1)	// error
+		perror("brk error");
+
+	show_program_break();
+
+#if 0	
 	p = sbrk(0);
-	printf("%p\n", p);
+	printf("%p\n", p);	// assume: v  (different by each run)
 	p = sbrk(0x100);
-	printf("%p\n", p);
+	printf("%p\n", p);	// v
+	p = sbrk(0);
+	printf("%p\n", p);	// v + 0x100
+#endif
+
     return 0;
 }
