1c554e929501f7b50b19a1ad2e0de93cfe1107b2
Xi Chen <511272827@qq.com>
2017-12-03 12:29:50 +0800

    trigraph
    Signed-off-by: Xi Chen <511272827@qq.com>

---
+++ c/_unexpect_c/trigraph.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#include "common_local.h"
+
+// build option: -trigraphs
+int main(int argc, char *argv[])
+{
+    printf("??)\n");
+    return 0;
+}
