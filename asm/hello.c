f7fd06dafe3333cbf70ae55bae0df15b101ee003
Xi Chen <511272827@qq.com>
2018-02-22 22:10:46 +0800

    asm
    Signed-off-by: Xi Chen <511272827@qq.com>

---
+++ asm/hello.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int k = 3;
+
+int main(int argc, char *argv[])
+{
+	int i, j;
+
+	i = 1;
+	j = 2;
+
+	k = i + j;
+
+	printf("%d\n", k);
+    return 0;
+}
---
+++ asm/smr.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ -z $1 ]
+then
+	echo "No argument..."
+	exit -1
+fi
+
+gcc -o $1.simple.elf $1
+
+if [ $? -eq 0 ]
+then
+./$1.simple.elf
+fi
