f729d328699f990a04eb6cb1e19aca0ab306721a
Xi Chen <511272827@qq.com>
2018-06-18 12:16:38 +0800

    remove files
    Signed-off-by: Xi Chen <511272827@qq.com>

--- c/xq/bi_search.h
+++
@@ -1,9 +0,0 @@
-#ifndef BI_SEARCH_H
-#define	BI_SEARCH_H
-
-
-int		bi_search(int a[], int v, int s, int e);
-int		bi_search_iter(int a[], int v, int s, int e);
-
-
-#endif
--- c/xq/common_local.h
+++
@@ -1,6 +0,0 @@
-#ifndef COMMON_LOCAL_H
-#define COMMON_LOCAL_H
-
-#include "../../../common/common.h"
-
-#endif
