52c14a8070d65ce197c2c797a4d44c93eb7023e7
Xi Chen <511272827@qq.com>
2018-10-04 20:48:17 +0800

    first csharp code: hello
    Signed-off-by: Xi Chen <511272827@qq.com>

---
+++ csharp/hello/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string max;
+
+            max = "hello, cat";
+            Console.WriteLine(max);
+
+            max = System.Console.ReadLine();
+            Console.WriteLine(max);
+        }
+    }
+}
