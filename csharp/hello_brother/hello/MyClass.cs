c4c33233657253a278c29271b1d93dc52351b51b
Xi Chen <511272827@qq.com>
2018-10-29 20:39:03 +0800

    hello brother project
    Signed-off-by: Xi Chen <511272827@qq.com>

---
+++ csharp/hello_brother/hello/MyClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace hello
+{
+    public class MyClass
+    {
+        public MyClass()
+        {
+        }
+
+        public static void Main1()
+        {
+            Console.WriteLine("MyClass main!\n");
+        }
+    }
+}
---
+++ csharp/hello_brother/hello/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using System.Net;
+
+namespace hello
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string url = "http://www.baidu.com/index.html";
+            string path = "/tmp/xxxxx.html";
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("maomao猫猫");
+
+            WebClient webClient = new WebClient();
+            webClient.DownloadFile(url, path);
+        }
+    }
+}
