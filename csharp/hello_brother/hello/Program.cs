5ab941f094f252855cbe3598f40cbab3ce1e0b73
Xi Chen <511272827@qq.com>
2018-10-29 21:01:13 +0800

    try catch throw;
    Signed-off-by: Xi Chen <511272827@qq.com>

--- csharp/hello_brother/hello/Program.cs
+++ csharp/hello_brother/hello/Program.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿//#define WEBCLIENT
+
+using System;
 
 using System.Net;
+
 
 namespace hello
 {
@@ -8,14 +11,43 @@ namespace hello
     {
         public static void Main(string[] args)
         {
+            /*
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("maomao猫猫");
+            */
+
+            test_try_catch();
+
+#if WEBCLIENT
             string url = "http://www.baidu.com/index.html";
             string path = "/tmp/xxxxx.html";
 
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("maomao猫猫");
-
             WebClient webClient = new WebClient();
             webClient.DownloadFile(url, path);
+#endif
+        }
+
+        private static void test_try_catch()
+        {
+            try
+            {
+                try
+                {
+                    throw new Exception("maomao exception");
+                }
+                catch (Exception e)
+                {
+                    System.Console.WriteLine(e.Message + e.StackTrace);
+                    throw;
+                }
+                finally
+                {
+                    System.Console.WriteLine("finally processed...");
+                }
+            } catch (Exception e)
+            {
+                System.Console.WriteLine(e.Message + e.StackTrace); 
+            }
         }
     }
 }
