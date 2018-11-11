6fdfded6a6a3db0876c88ae9494bdb69ebd8aaac
Xi Chen <511272827@qq.com>
2018-11-11 15:28:39 +0800

    ios single view demo
    Signed-off-by: Xi Chen <511272827@qq.com>

---
+++ ios/single_view/single_view/ViewController.h
@@ -0,0 +1,19 @@
+//
+//  ViewController.h
+//  snigle_view
+//
+//  Created by eyu on 2018/11/11.
+//  Copyright © 2018年 eyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+{
+    IBOutlet UITextView *tv;
+}
+
+- (IBAction)click:(id)sender;
+
+@end
+
---
+++ ios/single_view/single_view/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  snigle_view
+//
+//  Created by eyu on 2018/11/11.
+//  Copyright © 2018年 eyu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
