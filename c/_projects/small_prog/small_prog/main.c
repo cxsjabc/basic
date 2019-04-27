bbbe7ab8095733bfd6e8f10294b3377ce0c525a3
Xi Chen <511272827@qq.com>
2019-04-27 21:12:09 +0800

    smallest program
    Signed-off-by: Xi Chen <511272827@qq.com>

---
+++ c/_projects/small_prog/small_prog/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  small_prog
+//
+//  Created by eyu on 2019/4/27.
+//  Copyright © 2019年 eyu. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    
+    return 0;
+}
