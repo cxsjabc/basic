73d588eaa719a728a389c1c5f3e33c2446f1507a
Xi Chen <511272827@qq.com>
2019-07-21 07:51:36 +0800

    c basic test
    Signed-off-by: Xi Chen <511272827@qq.com>

---
+++ c/_topics/keyword.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#include "common_local.h"
+
+int main(int argc, char *argv[])
+{
+	int main = 1;
+	//int do = 1;	// compile error
+    return 0;
+}
---
+++ c/_topics/return.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#include "common_local.h"
+
+//int main(int argc, char *argv[])
+void main(int argc, char *argv[])
+{
+}
