9f3264d0169a281cb2a3b9d5c93980a8b0623c31
Xi Chen <511272827@qq.com>
2019-07-23 07:22:19 +0800

    func args
    Signed-off-by: Xi Chen <511272827@qq.com>

---
+++ c/_topics/func_vars.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#include "common_local.h"
+
+int main(int argc, char *argv[])
+{
+	int a, b, c;
+
+	PP(&a);
+	PP(&b);
+	PP(&c);
+    return 0;
+}
