030ebb2f02969e55423aeb42f3c9f997d66a3a0e
Xi Chen <511272827@qq.com>
2020-07-13 20:52:02 +0800

    stdin buffer test
    Signed-off-by: Xi Chen <511272827@qq.com>

---
+++ c/_topics/scanf/stdin_buffer.c
@@ -0,0 +1,41 @@
+/*
+   Xi Chen(511272827@qq.com)
+   cxsjabcabc
+*/
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+void check_stdin_buffer()
+{
+	FILE *fp = stdin;
+	unsigned char *p;
+	int size;
+
+	p = stdin->_p;
+	size = stdin->_bf._size;
+
+	write(1, "stdin buffer:", strlen("stdin buffer:"));
+	write(1, p, size);
+	write(1, "\n", 1);
+}
+
+int main(int argc, char *argv[])
+{
+	int n;
+
+	// stdin buffer should be empty now!
+	check_stdin_buffer();
+
+	// input one integer
+	scanf("%d", &n);
+	// current input buffer should be the extra string getting rid of the previous int
+	check_stdin_buffer();
+
+	// set the current input buffer character to 'X'
+	stdin->_p[0] = 'X';	
+	check_stdin_buffer();
+
+	return 0;
+}
